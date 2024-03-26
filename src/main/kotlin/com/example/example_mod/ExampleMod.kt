108db879e38167096a5588f9c88f03e4b09ea889
coolGi <sasanaps@hotmail.com>
2024-03-26 21:06:52 +1030

    Updated to use Slf4j rather than Java sysout

--- src/main/kotlin/com/example/example_mod/ExampleMod.kt
+++ src/main/kotlin/com/example/example_mod/ExampleMod.kt
@@ -2,14 +2,19 @@ package com.example.example_mod
 
 import org.coolcosmos.cosmicquilt.api.entrypoint.ModInitializer
 import org.quiltmc.loader.api.ModContainer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class ExampleMod : ModInitializer {
     companion object {
         const val MOD_ID = "example_mod"
+
+        @kotlin.jvm.JvmField
+        val LOGGER: Logger = LoggerFactory.getLogger("Example Mod")
     }
 
     override fun onInitialize(mod: ModContainer) {
-        println("Hello from $MOD_ID!")
+        LOGGER.info("Hello from $MOD_ID!")
     }
 }
 
